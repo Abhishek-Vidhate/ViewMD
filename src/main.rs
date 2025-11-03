da534905bcdb754361b0045d856cc678646dcdba
Abhishek Vidhate <aves@Abhisheks-MacBook-Air.local>
2025-11-03 10:08:00 +0530

    basic cli for reading file with  command implemented

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 // take file path input and read content of file in terminal
+use std::env;
 use std::{error::Error, fs};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let content = fs::read_to_string("test.md")?;
-    println!("content : {content}");
+    let command = env::args().nth(1).expect("no viewmd command provided");
+    let filepath = env::args().nth(2).expect("incorrect filepath");
+    // loop, when correct command provided
+    if command == "vmd" {
+        println!("command: {command} with filepath: {filepath}");
+
+        let content = fs::read_to_string(filepath)?;
+        println!("content : {content}");
+    } else {
+        println!("incorrect viewmd command provided");
+    }
+
     Ok(())
 }
